8c3ee7ace7d5204f679db3e0b9e869169ebb8411
rpasup <rajasekhar157@gmail.com>
2022-02-26 23:13:14 +0400

    added the all needful steps in the testing script-- update_script.sh

--- update_script.sh
+++ update_script.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+### clean up of the dynamic files ##
+rm finalfile.txt
+
 echo "getting the commit id"
 commit_id=`git rev-parse HEAD`
 echo "listing the files with the commit id : $commit_id"
@@ -15,8 +18,11 @@ while read targetline; do
 cat $line | grep $targetline
 if [ $? == 0 ]
 then
-   echo "we found the agent"
+   #echo "we found the agent"
    echo $targetline >> targent_agent_list.txt
+   target_agent=`cat mapping_files/preprod | grep $targetline | cut -d ":" -f2`
+   echo "target :$target_agent, source : $targetline"
+   sed -e 's/Source_SecureAgent_Name/'$targetline'/' -e 's/Target_SecureAgent_Name/'$target_agent'/' -e "s/comma/\,/" base_template >> finalfile.txt
 else
    echo "we didnt find the $targetline in $line"
 fi
@@ -24,3 +30,7 @@ done < $searchagent_list
 echo $?
 i=$((i+1))
 done < $commit_file
+
+sort targent_agent_list.txt | uniq > targent_agent.txt
+truncate -s -2 finalfile.txt
+sed '/#updatehere/r finalfile.txt' exisitingscript.txt > replacable_script.txt
