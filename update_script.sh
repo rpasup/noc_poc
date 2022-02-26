3b116a8169277bbbb70e1eb315f784e055570357
rpasup <rajasekhar157@gmail.com>
2022-02-26 19:49:42 +0400

    second test

--- update_script.sh
+++ update_script.sh
@@ -1,16 +1,26 @@
 #!/bin/sh
-
 echo "getting the commit id"
 commit_id=`git rev-parse HEAD`
 echo "listing the files with the commit id : $commit_id"
 `git show --pretty="" --name-only $commit_id > commit_id_files_list.txt`
 #cat commit_id_files_list.txt
 `cat mapping_files/dev | cut -d ":" -f1 > searchagent_list.txt`
-file='commit_id_files_list.txt'
+commit_file='commit_id_files_list.txt'
+searchagent_list='searchagent_list.txt'
 i=1
 while read line; do
 #Reading each line.
 echo "$i file : $line"
-cat $line | grep SecureAgent1
+while read targetline; do 
+cat $line | grep $targetline
+if [ $? == 0 ]
+then
+   echo "we found the agent"
+   echo $targetline >> targent_agent_list.txt
+else
+   echo "we didnt find the $targetline in $line"
+fi
+done < $searchagent_list
+echo $?
 i=$((i+1))
-done < $file
+done < $commit_file
